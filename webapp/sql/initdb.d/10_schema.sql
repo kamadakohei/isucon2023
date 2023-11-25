89c5d65f11f3d6595b5fa9bfc765394b31393e24
kamadakohei <kamada.math@gmail.com>
2023-11-25 15:34:13 +0900

    add index

--- webapp/sql/initdb.d/10_schema.sql
+++ webapp/sql/initdb.d/10_schema.sql
@@ -16,6 +16,7 @@ CREATE TABLE `icons` (
   `user_id` BIGINT NOT NULL,
   `image` LONGBLOB NOT NULL
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+ALTER TABLE icons ADD INDEX idx_user_id (user_id);
 
 -- ユーザごとのカスタムテーマ
 CREATE TABLE `themes` (
